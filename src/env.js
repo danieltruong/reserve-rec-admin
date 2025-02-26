e0deebec7649205f8e0443c365aba9321bc35100
Mark Lisé <mark@digitalspace.ca>
2025-02-25 16:19:43 -0800

    Updated OAuth domain in environment configuration
    - Changed `OAUTH_DOMAIN` from `reserve-rec-dev.auth.ca-central-1.amazoncognito.com` to `reserve-rec-admin-dev.auth.ca-central-1.amazoncognito.com`

--- src/env.js
+++ src/env.js
@@ -12,11 +12,10 @@
   window.__env.API_LOCATION = "https://6kbrprs1r7.execute-api.ca-central-1.amazonaws.com";
   window.__env.API_PATH = "/api";
   window.__env.GH_HASH = "local-build";
-  window.__env.OAUTH_DOMAIN = "reserve-rec-dev.auth.ca-central-1.amazoncognito.com";
   window.__env.USER_POOL_ID = "ca-central-1_7slkQMBQ9";
   window.__env.USER_POOL_CLIENT_ID = "5116tjrobjtubo0atj4f7k1m08";
   window.__env.IDENTITY_POOL_ID = "ca-central-1:dffcad14-ba1d-49f9-8a0c-1429c02a46a0";
-  window.__env.OAUTH_DOMAIN = "reserve-rec-dev.auth.ca-central-1.amazoncognito.com";
+  window.__env.OAUTH_DOMAIN = "reserve-rec-admin-dev.auth.ca-central-1.amazoncognito.com";
   window.__env.WEBSOCKET_URL = "wss://id7kh3hg1d.execute-api.ca-central-1.amazonaws.com/api/";
 
   // Add any feature-toggles
